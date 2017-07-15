04ea62570eedb706bf1759feb512503a661a293f
leon in 't Veld <leon3110l@gmail.com>
2017-07-15 02:22:27 +0200

    moved the server file and added express server code

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('client'));
+
+app.listen(PORT, function () {
+  console.log(`listening on port: ${PORT}!`);
+});
